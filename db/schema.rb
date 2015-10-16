7df1f91718856cdb24918f4d37053b55b3d48abd
ethanpitt <ekid777@gmail.com>
2015-10-16 14:18:52 -0700

    adding reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016192505) do
+ActiveRecord::Schema.define(version: 20151016204131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20151016192505) do
     t.text     "content"
     t.integer  "messager_id"
     t.integer  "messagee_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.integer  "reviewer_id"
+    t.integer  "reviewee_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
