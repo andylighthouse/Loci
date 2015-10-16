96011da17ef501a2116278366512012c11f3d61c
ethanpitt <ekid777@gmail.com>
2015-10-16 12:55:09 -0700

    Message table created and set relations to Users

---
+++ app/models/message.rb
@@ -0,0 +1,4 @@
+class Message < ActiveRecord::Base
+  belongs_to :messager, :class_name => 'User'
+  belongs_to :messagee, :class_name => 'User'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :sent_messages, :class_name => 'Message', :foreign_key => 'messager_id'
+  has_many :received_messages, :class_name => 'Message', :foreign_key => 'messagee_id'
 end
