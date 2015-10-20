f7ad4bf8d84be716005926f749bc20591f0e91d1
Andy Li <andylibc@gmail.com>
2015-10-19 17:28:26 -0700

    added ajax to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
+  
 
   def index
+    @users = User.all
   end
 
   def show
@@ -14,7 +16,38 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
+  def create
+
+    @user = User.new(user_params)
+    
+    if @user.save
+      session[:user_id] = @user.id
+      respond_to do |format|
+        format.json{render :json => @user} 
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address)
+  end
   
 end
+
+
+
+
+
+  
