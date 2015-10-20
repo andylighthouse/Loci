db809da37844404f1fd0dcb122c4a8c41f310d0e
ethanpitt <ekid777@gmail.com>
2015-10-20 13:25:12 -0700

    added seed of skill lists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def get_facebook_api_key
-   '964264406965794'
- end
-
- def get_facebook_secret
-   '50d161ff4d4be2ee65acb286f72170c5'
- end
-
 end
--- db/migrate/20151020193700_add_uid_to_users_table.rb
+++
@@ -1,4 +0,0 @@
-class AddUidToUsersTable < ActiveRecord::Migration
-  def change
-  end
-end
