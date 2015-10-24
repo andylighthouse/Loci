38a0393cbca66ed116715371c31352af0c0b230a
Andy Li <andylibc@gmail.com>
2015-10-23 20:57:56 -0700

    chat 99% done

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -2,13 +2,19 @@
   $(function() {
     $('.demo-chat-input').on('click', 'button', function() {
       var msg = $(this).siblings('#new-msg').val();
-      var room_id = $(this).siblings('input').data('id');
+      // var room_id = $(this).siblings('input').data('id');
+
+
+      // How are you going to determine the room name?
+      // using the current user id and the recipient id,
+      // sort by lowest first. generate a room name
+
       console.log(msg);
-      console.log('id', room_id);
+      console.log('sending message to '+ window.room);
       $.ajax({
         url: '/send_message',
         method: 'POST',
-        data: {message: msg}
+        data: {message: msg, room: window.room}
       });
     });
   });
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,20 +2,15 @@ class ChatsController < ApplicationController
 
   # before_action :check_if_allowed_in_room
   def message
-    # binding.pry
-    # room_id = params[:id]
-    # begin
-    #   pusher = Pusher.trigger("#{room_id}", 'message', {:some => 'data'})
-    #   binding.pry
-    # rescue Pusher::Error => e
-    # # (Pusher::AuthenticationError, Pusher::HTTPError, or Pusher::Error)
-    # end
   end
 
   def send_message
     message = params[:message]
-    room_id = params[:id]
-    pusher = Pusher.trigger('1', 'message', {:message => message})
+    room = params[:room]
+
+
+    # user id 
+    pusher = Pusher.trigger(room, 'message', {:message => message})
     respond_to do |format|
       format.json { render :json => pusher }
     end
